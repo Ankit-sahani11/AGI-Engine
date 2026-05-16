b21af7879beb94ac1f57982ceb7f159fcb13d868
Ankit <anilsahaniqa@gmail.com>
2026-05-16 18:11:16 +0530

    add thread

--- Vulkan.cpp
+++ Vulkan.cpp
@@ -1,4 +1,6 @@
 #define VK_USE_PLATFORM_ANDROID_KHR
+#include <chrono>
+#include <thread>
 #include <fstream>
 #include<string>
 #include<iostream> 
@@ -826,12 +828,18 @@ VkFence inFlightFence;// aur check karata hai ki hawa me drawing processing to n
         // Logic final tep parda hat gaya aur mera triangle screen pr aa gaya 
         vkQueuePresentKHR(graphicsQueue, &presentInfo);
     }
-    void While_true(){
-       // Jab tak app band nahi hoti, triangle draw karte raho
-       while (true) { 
-           Frame_draw(); 
-       }
+    void While_true() {
+       float fps = 30.0;// fps set kiya 
+       int sleep_time = static_cast<int>(1000.0 / fps);// tyep cast kiya approx 33 mili second  
+       while (true) {// loop start kiya 
+       std::thread fps_controller(Frame_draw);// thread on kiya
+       std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));// time zone set kiya 
+       if (fps_controller.joinable()) {// ye chack kiya ki ye thread active thread hai 
+           fps_controller.join(); // agar ha to join kar do
+        }return;
     }
+}
+
 };
 
 int main(){
